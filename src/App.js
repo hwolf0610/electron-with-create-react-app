77c4bfd746f0b941cff10010cf4860f823c9ff43
hwolf0610 <hwolf0610@gmail.com>
2022-01-24 14:50:35 +0800

    add welcome page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import WelcomePage from './Pages/WelcomePage'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron 123456</h2>
-        </div>
-        <p className="App-intro">
-          Hello Electron!
-        </p>
-      </div>
+      <WelcomePage />
     );
   }
 }
